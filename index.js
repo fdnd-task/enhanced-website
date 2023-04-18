a9fd93fbf75bcb3b6bd5948c98e36f1a667e582c
iBadr49 <badr.amara@hva.nl>
2023-04-18 13:16:14 +0200

    Opzet files

---
+++ index.js
@@ -0,0 +1,24 @@
+// Express uit de nodemodules map
+import express from 'express'
+
+// Maak een nieuwe express app aan
+const app = express()
+
+//  Stel in hoe we express gebruiken
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+// Gebruik maken van de "public" map 
+app.use(express.static('public'))
+
+// Maak een route voor de index
+app.get('/', function (req, res) {
+  // res.send('Hello World!')
+  res.render('index')
+})
+
+// Stel het poortnummer in en start express
+app.set('port', process.env.PORT || 8000)
+app.listen(app.get('port'), function () {
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
