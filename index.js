3ad4eddec8e0da4d4431e74e07b2c51c4471fd07
DikkeTimo <62908209+DikkeTimo@users.noreply.github.com>
2023-04-19 12:23:58 +0200

    #5 done
    import the urls

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ import https from "https";
 // Create a new Express app
 const app = express();
 
+const url = ["https://raw.githubusercontent.com/fdnd-agency/ultitv/main/ultitv-api"];
+const postUrl = "https://api.ultitv.fdnd.nl/api/v1/players?first=20";
+const apiUrl = "https://api.ultitv.fdnd.nl/api/v1/questions";
+
+const urls = [
+  [url] + "/game/943.json",
+  [url] + "/game/943/statistics.json",
+  [url] + "/facts/Player/8607.json",
+  [postUrl],
+  [apiUrl],
+];
+
 // Set EJS as the template engine and specify the views directory
 app.set("view engine", "ejs");
 app.set("views", "./views");
@@ -16,10 +28,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async function (request, response) {
-  response.render("index");
+  const [data1, data2, data3, data4, data5] = await Promise.all(urls.map(fetchJson));
+  const data = { data1, data2, data3, data4, data5 };
+
+  response.render("index", data);
+  console.log(data);
 });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Application available on: http://localhost:${port}`);
 });
+
+async function fetchJson(urls) {
+  return await fetch(urls)
+    .then((response) => response.json())
+    .catch((error) => error);
+}
