adae008f4e26309c15adfd41801b52f8ef1b156e
Amir Napoletano <amirnapoletano@gmail.com>
2023-05-04 17:45:59 +0200

    reserveren pagina en success pagina gebouwd
    Van de reserveren pagina kan je gemakkelijk naar de success pagina gaan  en van de success pagina kan je gemakkelijk via een knop naar de home pagina

--- index.js
+++ index.js
@@ -1,48 +1,49 @@
-import express from 'express'
+import express from "express";
 
 // const url = "https://zoeken.oba.nl/api/v1/search/";
 
-
 // Maak een nieuwe express app
-const app = express()
+const app = express();
 
 // Stel in hoe we express gebruiken
-app.set('view engine', 'ejs')
-app.set('views', './views')
-app.use(express.static('public'))   
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static("public"));
 
 // Stel de afhandeling van formulieren in
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Maak een route voor de index pagina
-app.get('/', (request, response) => {
-
-    fetchJson().then((data) => {
-        response.render('index', data)
-    })
-   
-})
+app.get("/", (request, response) => {
+  fetchJson().then((data) => {
+    response.render("index", data);
+  });
+});
 
 // Maak een route voor de detail pagina
 app.get("/detail", (request, response) => {
-    fetchJson().then((data) => {
-        response.render('detail', data)
-    })
+  fetchJson().then((data) => {
+    response.render("detail", data);
+  });
+});
+app.get("/success", (request, response) => {
+  fetchJson().then((data) => {
+    response.render("success", data);
+  });
+});
+app.get("/reserveren", (request, response) => {
+  fetchJson().then((data) => {
+    response.render("reserveren", data);
+  });
 });
 
 
-app.get('succes', (request, response) => {
-    fetchJson().then((data) => {
-        response.render('succes', data)
-    })
-})
-
 // Stel het poortnummer in en start express
-app.set('port', process.env.PORT || 8000)
-app.listen(app.get('port'), function () {
-  console.log(`Application started on http://localhost:${app.get('port')}`)
-})
+app.set("port", process.env.PORT || 8000);
+app.listen(app.get("port"), function () {
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
 
 /**
  * Wraps the fetch api and returns the response body parsed through json
@@ -52,15 +53,14 @@ app.listen(app.get('port'), function () {
 async function fetchJson(url) {
   return await fetch(url)
     .then((response) => response.json())
-    .catch((error) => error)
+    .catch((error) => error);
 }
 export async function postJson(url, body) {
   return await fetch(url, {
-    method: 'post',
+    method: "post",
     body: JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json' },
+    headers: { "Content-Type": "application/json" },
   })
     .then((response) => response.json())
-    .catch((error) => error)
+    .catch((error) => error);
 }
-
