481536db2c31f374642f3422269e840dcafe45d0
Amberhva <amber.van.der.reijden@hva.nl>
2023-05-08 10:47:59 +0200

    Error is fixed
    fixed the error in js

--- index.js
+++ index.js
@@ -21,56 +21,31 @@ server.get("/", (request, response) => {
     const categoriesUrl = url + `/categories`;
 
     fetchJson(categoriesUrl).then((data) => {
-      response.render('index', data)
-    })
-  })
+        response.render("index", data);
+    });
+});
 
-  // test route voor test pagina roelie producten
-  server.get('/pinda-ei', async (request, response) => {
-    const query = request.query.categorieId
+// test route voor test pagina roelie producten
+server.get("/pinda-ei", async (request, response) => {
+    const query = request.query.categorieId;
 
-    const productenUrl = url + `/producten?categorieId=${query}`
-  
+    const productenUrl = url + `/producten?categorieId=${query}`;
+
     await fetchJson(productenUrl).then((data) => {
-      response.render('pinda-ei', {data: data} )
-    })
-  })
+        response.render("pinda-ei", { data: data });
+    });
+});
 
 // Route voor de producten pagina
-  server.get('/product-pagina', async (request, response) => {
- 
-    const query = request.query.id
+server.get("/product-pagina", async (request, response) => {
+    const query = request.query.id;
 
-    const productenUrl = url + `/product?id=${query}`
-  
+    const productenUrl = url + `/product?id=${query}`;
+
     await fetchJson(productenUrl).then((data) => {
-      response.render('product-pagina', {data: data} )
-    })
-  })
-  
-  // Route voor de checkout pagina
-  server.get('/checkout', async (request, response) => {
- 
-    const query = request.query.id
-
-    const productenUrl = url + `/product?id=${query}`
-  
-    await fetchJson(productenUrl).then((data) => {
-      response.render('checkout', {data: data} )
-    })
-  })
-
-    // definieer de fetchJson functie
-    async function fetchJson(url) {
-        return await fetch(url)
-          .then((response) => response.json())
-          .catch((error) => error)
-      }
-      
-      // Start met luisteren
-    server.listen(server.get('port'), () => {
-        console.log(`Application started on http://localhost:${server.get('port')}`)
-      })
+        response.render("product-pagina", { data: data });
+    });
+});
 
 // Route voor de checkout pagina
 server.get("/checkout", async (request, response) => {
@@ -94,4 +69,3 @@ async function fetchJson(url) {
 server.listen(server.get("port"), () => {
     console.log(`Application started on http://localhost:${server.get("port")}`);
 });
-
