338cb83abaceeed3e000444d6d01733966ab733c
Daniquedejong <danique.de.jong3@hva.nl>
2023-05-08 11:58:36 +0200

    producten pagina & footer

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
 // Express uit de nodemodules map
-import express from 'express'
+import express from "express";
+
+const url = "https://api.vinimini.fdnd.nl/api/v1/producten"; // URL naar Json data
+const url2 = 'https://api.vinimini.fdnd.nl/api/v1';
 
 // Maak een nieuwe express app aan
-const app = express()
+const app = express();
 
 //  Stel in hoe we express gebruiken
-app.set('view engine', 'ejs')
-app.set('views', './views')
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-// Gebruik maken van de "public" map 
-app.use(express.static('public'))
+// Gebruik maken van de "public" map
+app.use(express.static("public"));
 
 // Maak een route voor de index
-app.get('/', function (req, res) {
-  // res.send('Hello World!')
-  res.render('index')
-})
+app.get("/", (request, response) => {
+  let productenUrl = url;
 
-// Stel het poortnummer in en start express
-app.set('port', process.env.PORT || 8000)
-app.listen(app.get('port'), function () {
-  console.log(`Application started on http://localhost:${app.get('port')}`)
-})
+  fetchJson(productenUrl).then((data) => {
+    response.render("producten", data);
+  });
+});
+
+// Detail pagina
+app.get("/detail", (request, response) => {
+  let id = request.query.detailId || "clerps05z09jm0aw3vccjq5un";
+  let detailUrl2 = url2 + "/product?id=" + id;
+  console.log(detailUrl2);
+  fetchJson(detailUrl2).then((data) => {
+    // console.log(data)
+    response.render("detail", data);
+  });
+});
+
+// Stel het poortnummer in waar express op gaat luisteren
+app.set("port", process.env.PORT || 8000);
+
+// Start express op, haal het ingestelde poortnummer op
+app.listen(app.get("port"), function () {
+  // Toon een bericht in de console en geef het poortnummer door
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
+
+async function fetchJson(url, payload = {}) {
+  return await fetch(url, payload)
+    .then((response) => response.json())
+    .catch((error) => error);
+}
