3303b3224fc1130df2bab9503673d6b2038e26f6
Youssra Elmortai <112857270+zombie0youssra@users.noreply.github.com>
2023-05-10 16:47:26 +0200

    Revert "Detailpagina"

--- public/client-side.js
+++ public/client-side.js
@@ -1,14 +1,3 @@
-
-// laad meer knop
-
-// lees meer functie detailpagina
-// Dit stuk zoekt de togglebutton en zorgt ervoor dat hij de functie eronder activeerd wanneer erop geklikt wordt
-let stappenplan = document.getElementById("stappenplanbutton");
-stappenplan.addEventListener("click", toonStappenplan);
-// Dit deel toggled de class "toon" waarop display none staat
-function toonStappenplan() {
-  document.getElementById("stappen").classList.toggle("toon");
-  
 // filter
 
 const searchBar = document.getElementById("site-search");
