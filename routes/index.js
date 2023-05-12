5991bea8d7c145075f902b0c4a69d109ace3dece
MokhtarAkle <mokhtar.akle@hva.nl>
2023-05-12 10:53:13 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -22,29 +22,6 @@ router.get('/', function(req, res, next) {
 });
 
 
-// router.post('/', (request, response) => {
-  
-
-});
-
-  
-
-//   postJson(url1, request.body).then((data) => {
-//     let newZone = { ... request.body }
-//     console.log(data)
-//     if (data.success) {
-//       response.redirect('/?memberPosted=true') 
-//       // TODO: squad meegeven, message meegeven
-//       // TODO: Toast meegeven aan de homepagina
-
-//     } else {
-//       const errormessage = `${data.message}: Mogelijk komt dit door de slug die al bestaat.`
-//       const newdata = { error: errormessage, values: newZone }
-
-//       }
-//   })
-// })
-
 router.post('/', (request, response) => {
   request.body.timeStart = request.body.dateStart + 'T' + request.body.timeStart + ':00Z';
   request.body.timeEnd = request.body.dateEnd + 'T' + request.body.timeEnd + ':00Z';    
